79fc271b868714d9432af3f3131117f3cca2452c
Muhammad Ahsan <ahsanexpert16@gmail.com>
2025-12-18 13:50:43 +0500

    Custom Pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { StudentModule } from './student/student.module';
 import { CustomerModule } from './customer/customer.module';
+import { MynameController } from './myname/myname.controller';
 
 @Module({
   imports: [StudentModule, CustomerModule],
-  controllers: [AppController, UserController, ProductController],
+  controllers: [AppController,  ProductController, MynameController],
   providers: [AppService, ProductService],
 })
 export class AppModule {}
