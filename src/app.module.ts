764a7398d3cfaa0dae0056e65234eff6471740fd
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-01-06 13:01:54 +0500

    Middleware in Nest Js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductService } from './product/product.service';
@@ -8,10 +8,15 @@ import { CustomerModule } from './customer/customer.module';
 import { MynameController } from './myname/myname.controller';
 import { UserRolesController } from './user-roles/user-roles.controller';
 import { ExceptionController } from './exception/exception.controller';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [StudentModule, CustomerModule],
   controllers: [AppController,  ProductController, MynameController, UserRolesController, ExceptionController],
   providers: [AppService, ProductService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+   configure(consumer: MiddlewareConsumer) {
+     consumer.apply(LoggerMiddleware).forRoutes('*');
+   }
+}
